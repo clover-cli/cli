ab2872f8b18d0d81171b014c285501db03cf670d
andresdanielmtz <autummata@gmail.com>
2026-09-23 23:52:03 -0700

    remove: unused options on yargs setup

--- src/args.ts
+++ src/args.ts
@@ -1,24 +1,13 @@
 import yargs from 'yargs';
-import awsCommand from './commands/aws';
+import awsCommand from './commands/aws/aws';
 
 /**
  * Argument and command definitions.
- * Each provider's commands live in src/commands/<provider>.ts
+ * Each provider's commands live in src/commands/<provider>/<provider>.ts
  */
 const argv = yargs(process.argv.slice(2))
     .scriptName('clover')
     .command(awsCommand)
-    .options({
-        a: {
-            type: 'boolean', default: false
-        },
-        b: {
-            type: 'number', default: false
-        },
-        message: {
-            type: 'string', default: false
-        }
-    })
     .strict()
     .help()
     .parse();
